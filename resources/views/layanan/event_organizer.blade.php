7a52d978660b79d03c1ff7f644ca793c8bd8e65e
Aldo Syahputra <aldosyahputra80@gmail.com>
2024-12-19 15:21:47 +0700

    membuat tampilan halaman event organizer

--- resources/views/layanan/event_organizer.blade.php
+++ resources/views/layanan/event_organizer.blade.php
@@ -1,6 +1,121 @@
-<x-layout :css="['acara_khusus.css', 'additional.css']">
-    <div class="container mt-5">
-        <h1>Acara Khusus</h1>
-        <p>Detail acara khusus yang diadakan perusahaan.</p>
+<x-layout :navbar="null" :css="'/layanan/event_organizer.css'">
+    <div class="description-page">
+        <h5>Layanan</h5>
+        <h4>Event Organizer</h4>
+    </div>
+    <div class="intro-event-section p-3 p-lg-5 mt-5 my-lg-5 d-flex flex-column flex-lg-row align-items-center">
+        <div class="intro-img-section d-flex flex-row align-items-center">
+            <img src="/img/layanan/event-organizer/event2.png" alt="">
+            <img src="/img/layanan/event-organizer/event3.png" alt="">
+        </div>
+        <div class="intro-text-section p-3 p-lg-5 mt-3 mt-lg-0">
+            <p><span class="fw-bold">Cendana Solution Center (CSC)</span> memfasilitasi kegiatan bidang pendidikan, Bimbingan Teknis, Diklat, Seminar, Workshop, Traning, Outbond, sertifikasi, Kunjungan Kerja, Study Tiru, Study Banding, dan Konsultasi melalui dua lembaga pelaksana kami yaitu PT.PUSDIKLATNAS dan PT.PUSTAKAPEMDA dengan narasumber yang berkualitas dan mempuyai keahlian khusus/spesialis serta profesional sesuai bidangnya. Kualitas dan metode pengajaran, baik isi materi maupun tenaga pengajar dilakukan dengan menggunakan metode yang terbaru sehingga mudah dimengerti oleh peserta.</p>
+        </div>
+    </div>
+    <div class="bidang-event-section p-3 p-lg-5">
+        <div class="blue-bar"></div>
+        <h3>Bidang Kami</h3>
+        <ol>
+            <li>Peraturan perundang-undangan yang diterbitkan /dikeluarkan oleh kementerian maupun pemerintah meliputi : <br>
+                Permendes Nomor 11 Tahun 2019 Tentang Prioritas Penggunaan dana Desa Tahun 2020</li>
+                <ul>
+                    <li>Pemanfaatan Dana Desa Tahun 2020</li>
+                    <li>Pedoman Umum Pelaksanaan Penggunaan Dana Desa Tahun 2020</li>
+                    <li>Desa Siaga Bidang Kesehatan Berbasis Masyaraka</li>
+                    <li>Percepatan Peningkatan SDM Di Desa dengan Platform Digital</li>
+                    <li>Undang-Undang No.3 Tahun 2024 Tentang Desa</li>
+                </ul>
+            <li>Sosialisasi berbagai peraturan terbaru beserta isu-isu strategis yang berkembang</li>
+            <li>Meningkatkan kesadaran dan pengetahuan tentang pentingnya gizi seimbang</li>
+            <li>Peningkatan Kapasitas Kewenangan Desa antara lain :</li>
+            <ul>
+                <li>Permendesa dan RPMJ Desa</li>
+            </ul>
+            <li>Mewujudkan Tata kelola Pemerintahan Desa Sesuai UU No. 3 Tahun 2024 Tentang Desa</li>
+            <div class="d-flex flex-column align-items-center align-items-lg-start">
+                <div class="collapse" id="readMore">
+                    <li>Peningkatan Perekonomian rakyat melalui potensi desa</li>
+                    <li>Peningkatan Skill anggota legislatif agar mampu memerankan fungsilegislasi, controling, dan Budgeting di dewan nanti</li>
+                    <li>Pengadaan Barang dan Jasa</li>
+                    <li>Barang dan Aset</li>
+                    <li>Kearsipan dan Perpustakan</li>
+                    <li>Pekerjaan Umum (PU)</li>
+                    <li>Kehumasan dan Protokole</li>
+                    <li>Satpol PP</li>
+                    <li>Pertambangan</li>
+                    <li>Sistem Informasi Geografi</li>
+                    <li>Penanaman Modal</li>
+                    <li>Rumah Sakit, BUMN dan BUMD</li>
+                    <li>Tata Usaha (TU)</li>
+                    <li>Bendahara</li>
+                    <li>Internet of Thing (Iot) 4.0</li>
+                    <li>Tata Kota dan Lingkungan hidup</li>
+                    <li>Koperasi dan UMKM</li>
+                    <li>Kependudukan dan Catatan Sipil</li>
+                    <li>Sekretaris Daerah</li>
+                    <li>Keuangan</li>
+                    <li>DPRD dan Setwan</li>
+                    <li>Pariwisata</li>
+                    <li>Kepegawaian</li>
+                    <li>Kesehatan</li>
+                    <li>Perpajakan</li>
+                    <li>Outbond</li>
+                </div>
+            </div>
+        </ol>
+        <a class="btn btn-link p-0 toggle-btn d-flex align-items-center mt-2 mx-auto mx-lg-0" data-bs-toggle="collapse" href="#readMore" role="button" aria-expanded="false" aria-controls="readMore">
+            <span class="toggle-text me-2"></span> <i class="bi bi-chevron-down toggle-icon"></i>
+        </a>
+    </div>
+    <div class="keunggulan-event-section p-3 p-lg-5">
+        <div class="blue-bar"></div>
+        <div class="keunggulan-text-section">
+            <h3>Keunggulan Kami</h3>
+            <ul>
+                <li>Pengalaman dan Jaringan Luas: Selama lebih dari dua dekade, kami telah bekerja dengan berbagai instansi pemerintah dan perusahaan di seluruh Indonesia, memberikan kami keunggulan dalam memahami kebutuhan klien</li>
+                <li>Pendekatan Personal: Setiap acara dirancang khusus sesuai dengan visi klien, memastikan hasil yang maksimal dan memuaskan.</li>
+                <li>Tim Profesional: Dengan tim yang berpengalaman dan berdedikasi, kami memastikan setiap detail acara berjalan lancar.</li>
+                <li>Inovasi dan Kreativitas: Kami selalu mencari cara baru untuk membuat acara Anda unik dan berkesan.</li>
+            </ul>
+        </div>
+    </div>
+    <div class="gallery-event-section d-flex flex-wrap justify-content-center align-items-center gap-5">
+        <img src="/img/layanan/event-organizer/event-gallery-1.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-2.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-3.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-4.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-5.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-6.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-7.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-8.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-9.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-10.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-11.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-12.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-13.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-14.png" alt="">
+        <img src="/img/layanan/event-organizer/event-gallery-15.png" alt="">
+    </div>
+    <div class="layanan-lainnya-section d-flex flex-column flex-lg-row justify-center align-items gap-2 gap-lg-5 my-5 p-5">
+        <a href="">
+            <div class="card position-relative mb-4">
+                <img src="/img/layanan/sawah.jpg" class="card-img-top" alt="...">
+                <div class="card-body position-absolute ">
+                    <div class="line-bar mb-2"></div>
+                    <p class="fs-3 fw-bold">Ketahanan Pangan</p>
+                    <a href="#" class="">Lebih lanjut...</a>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="card position-relative mb-4">
+                <img src="/img/layanan/konstruksi.jpg" class="card-img-top" alt="...">
+                <div class="card-body position-absolute ">
+                    <div class="line-bar mb-2"></div>
+                    <p class="fs-3 fw-bold">Kontruksi</p>
+                    <a href="#" class="">Lebih lanjut...</a>
+                </div>
+            </div>
+        </a>
     </div>
 </x-layout>
