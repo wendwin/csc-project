33c8dc170c0c9e75d490a6881bccc67480a37324
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-03 05:06:48 +0700

    membuat component struktur domain pustaka pemda

---
+++ resources/views/pustakapemda-components/navbar.blade.php
@@ -0,0 +1,16 @@
+{{-- INI NAVBAR UNTUK WEBISTE PUSTAKA PEMDA, SILAHKAN DISESUIKAN. INGET!!! HARUS CLINE CODE --}}
+{{-- HANDLE BY ALDO OR FAISAL--}}
+
+<nav class="bg-white shadow-md">
+          <div class="container mx-auto px-4 py-4 flex justify-between items-center">
+              <div class="text-xl font-bold text-blue-600">
+                  Pustaka Pemda
+              </div>
+              <ul class="flex space-x-6 text-gray-700 font-medium">
+                  <li><a href="/" class="hover:text-blue-600">Beranda</a></li>
+                  <li><a href="/tentang" class="hover:text-blue-600">Tentang</a></li>
+                  <li><a href="/kontak" class="hover:text-blue-600">Kontak</a></li>
+              </ul>
+          </div>
+      </nav>
+      
