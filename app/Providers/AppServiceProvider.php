064f0b944f842bd76f3d2debe6bfd08b74bc8724
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-03 05:09:38 +0700

    mengatur auth untukk manage user login dan rederch automatic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\Authenticate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Authenticate::redirectUsing(function ($request) {
+            return route('admin.login');
+        });
     }
+
 }
