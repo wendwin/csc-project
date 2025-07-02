18b00a4bf06a87d268032d43fb42db414ab72a44
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-03 05:05:31 +0700

    membuat view stuktur untuk domian pspi component

---
+++ resources/views/pspi-components/navbar.blade.php
@@ -0,0 +1,16 @@
+{{-- INI NAVBAR UNTUK WEBISTE PSPI, SILAHKAN DISESUIKAN. INGET!!! HARUS CLINE CODE --}}
+{{-- HANDLE BY ALDO OR FAISAL--}}
+
+<nav class="bg-white shadow">
+          <div class="container mx-auto px-4 py-4 flex justify-between items-center">
+              <div class="text-xl font-bold text-green-600">
+                  PSPI
+              </div>
+              <ul class="flex space-x-6 font-medium text-gray-700">
+                  <li><a href="/pspi" class="hover:text-green-600">Beranda</a></li>
+                  <li><a href="/pspi/tentang" class="hover:text-green-600">Tentang</a></li>
+                  <li><a href="/pspi/kontak" class="hover:text-green-600">Kontak</a></li>
+              </ul>
+          </div>
+      </nav>
+      
