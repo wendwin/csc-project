5073bb7024e0c916c95eb898db45d1e9c35fe28b
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-03 05:06:05 +0700

    membuat view pspi

---
+++ resources/views/pspi/index.blade.php
@@ -0,0 +1,15 @@
+{{--INI HALAMAN HOME PSPI SILAHKAN SESUIKAN. INGET!!! WAJIB CLINE CODE  --}}
+{{-- HANDLE BY ALDO OR FAISAL--}}
+
+@extends('layouts.pspilayout')
+
+@section('content')
+<div class="text-center">
+    <h1 class="text-3xl font-bold text-green-700 mb-4">
+        Selamat Datang di Website Pusat Sertifikasi Profesi Indonesia
+    </h1>
+    <p class="text-lg text-gray-700">
+        Menyediakan informasi seputar sertifikasi profesi dan pengembangan kompetensi di Indonesia.
+    </p>
+</div>
+@endsection
