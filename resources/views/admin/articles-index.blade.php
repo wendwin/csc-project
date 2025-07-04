2c1f0bd62812ce2abecd0e38c1bcfee170cf6add
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-05 04:32:38 +0700

    menambahkan artcile index sebagai render view utama dashboard index artcile managenya

---
+++ resources/views/admin/articles-index.blade.php
@@ -0,0 +1,8 @@
+{{-- resources/views/articles/index.blade.php --}}
+@extends('layouts.adminlayout')
+
+@section('content')
+    <div class="p-6 bg-white rounded-lg shadow">
+        <h1 class="text-2xl font-semibold text-gray-800">Selamat Datang di Daftar Artikel</h1>
+    </div>
+@endsection
