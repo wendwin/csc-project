68094599f9ba0f21aa0fbac07d871b2f2125651b
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-08 06:44:04 +0700

    memperbarui layout admin dashboard

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -7,38 +7,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Dashboard' }}</title>
+
     {{-- global css & js --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    {{-- alphine js --}}
+
+    {{-- Alpine.js --}}
     <script src="//unpkg.com/alpinejs" defer></script>
-    <!-- Trix Editor CSS -->
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
-    <!-- Trix Editor JS -->
-    <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+
+    {{-- Trix Editor --}}
+    <link rel="stylesheet" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
+    <script src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
 
     <style>
         [x-cloak] {
             display: none !important;
         }
     </style>
-
 </head>
 
-<body class="bg-gray-100 flex" x-data="{ sidebarOpen: true }">
+<body class="bg-[#F5F6FA] flex flex-col md:flex-row min-h-screen" x-data="{ sidebarOpen: true }">
 
     {{-- Sidebar --}}
     @include('admin-components.sidebar')
 
-    <div class="flex flex-col flex-1">
+    <div class="flex-1 flex flex-col min-h-screen overflow-x-hidden">
         {{-- Navbar --}}
         @include('admin-components.navbar')
 
         {{-- Content --}}
-        <main class="p-6">
+        <main class="flex-1 p-4 sm:p-6 w-full">
             @yield('content')
         </main>
     </div>
-
 
 </body>
 
