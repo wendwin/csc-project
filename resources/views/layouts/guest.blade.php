39b38f6cef60d433a4a533d835f4d936b35cd648
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-08 06:44:32 +0700

    memperbarui lyout login view

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,6 +1,3 @@
-{{-- LOGIN  PERSONAL LAYOUT --}}
-{{-- HANDLE BY PUTRA --}}
-
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -9,10 +6,22 @@
     <title>{{ $title ?? 'Login Admin' }}</title>
     @vite('resources/css/app.css')
     <script src="https://unpkg.com/lucide@latest"></script>
-
+    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
-<body class="bg-gradient-to-br from-blue-100 to-white min-h-screen flex items-center justify-center">
-    <main class="w-full max-w-md p-6">
+<body class="relative bg-[#4880FF] min-h-screen flex items-center justify-center overflow-hidden">
+
+  {{-- Sudut Gambar --}}
+  <img src="{{ asset('img/logindashboard/stylebg/1.png') }}" class="absolute top-0 left-0 max-w-[600px] h-auto z-0" alt="Sudut Kiri Atas">
+  <img src="{{ asset('img/logindashboard/stylebg/2.png') }}" class="absolute top-0 right-0 max-w-[600px] h-auto z-0" alt="Sudut Kanan Atas">
+  <img src="{{ asset('img/logindashboard/stylebg/3.png') }}" class="absolute bottom-0 left-0  max-w-[400px] h-auto z-0" alt="Sudut Kiri Bawah">
+  <img src="{{ asset('img/logindashboard/stylebg/4.png') }}" class="absolute bottom-0 right-0 max-w-[200px] h-auto z-0" alt="Sudut Kanan Bawah">
+
+  {{-- Lapisan biru transparan di atas gambar --}}
+  <div class="absolute inset-0 bg-blue-500/20 z-10"></div>
+
+
+    {{-- Konten Utama --}}
+    <main class="relative z-10 w-full max-w-md">
         @yield('content')
     </main>
 </body>
