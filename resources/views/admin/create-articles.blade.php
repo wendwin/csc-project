ff4a328e403f6f01a061d6af81b5f7d5b9f88a10
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-07-10 06:19:12 +0700

    modifikasi artcile form kedua

--- resources/views/admin/create-articles.blade.php
+++ resources/views/admin/create-articles.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.adminlayout')
 
 @section('content')
-<div class="p-6 bg-white rounded-lg shadow">
-    <h2 class="text-2xl font-bold  mb-4 mr-4">Tambah Artikel</h2>
     
     @include('admin._form', [
         'action' => route('articles.store'),
         'method' => 'POST',
     ])
-</div>
 @endsection
