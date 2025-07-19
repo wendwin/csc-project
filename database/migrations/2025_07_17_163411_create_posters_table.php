de2503aa8ecfb93e6de632086eea16e9a835ca76
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-07-19 09:25:26 +0700

    memperbarui migrasi tabel poster

--- database/migrations/2025_07_17_163411_create_posters_table.php
+++ database/migrations/2025_07_17_163411_create_posters_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('image_path');
+            $table->string('target_website');
             $table->timestamps();
         });
     }
