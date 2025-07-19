8a4ddc01d0536aa85b197e1ed42490bff626e864
putra-pkwl03 <putrapongkowulu@gmail.com>
2025-07-19 09:24:55 +0700

    menambahkan kolom target website di model

--- app/Models/Poster.php
+++ app/Models/Poster.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Poster extends Model
 {
-    protected $fillable = ['title', 'image_path'];
+    protected $fillable = ['title', 'image_path', 'target_website'];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class, 'target_website', 'target_website');
+    }
 }
