488f552c9d43215c0b92e3e1eb7e328249bce6cc
Aldo Syahputra <aldosyahputra80@gmail.com>
2025-07-21 22:10:42 +0700

    menyelesaikan halaman profil

---
+++ resources/views/pspi/profil.blade.php
@@ -0,0 +1,18 @@
+{{--INI HALAMAN HOME PSPI SILAHKAN SESUIKAN. INGET!!! WAJIB CLINE CODE  --}}
+{{-- HANDLE BY ALDO OR FAISAL--}}
+
+@extends('layouts.pspilayout')
+
+@section('content')
+<div class="text-center">
+    <div class="bg-white">
+        <div class="relative -mb-[100px]">
+            <img src="/img/pspi/detail_berita.webp" alt="" class="w-full h-[250px] relative">
+            <h1 class="absolute text-xl md:text-2xl text-white uppercase font-bold top-5 left-5 md:top-10 md:left-10">Profil Kami</h1>
+        </div>
+    </div>
+    @include('pspi-components.landingpage.tentang')
+    @include('pspi-components.profil.mengapa')
+    @include('pspi-components.profil.visi_mitra')
+    @include('pspi-components.profil.deskripsi')
+@endsection
