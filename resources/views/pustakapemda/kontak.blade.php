d0aaf01a1d6cb2644ae7a369a38d5a630bb80c45
wendwin <wendwin12@gmail.com>
2025-07-25 21:06:19 +0700

    memperbaiki maps kontak

--- resources/views/pustakapemda/kontak.blade.php
+++ resources/views/pustakapemda/kontak.blade.php
@@ -1,4 +1,4 @@
-{{--INI HALAMAN HOME PUSTAKA PEMDA SILAHKAN SESUIKAN. INGET!!! WAJIB CLINE CODE --}}
+{{-- INI HALAMAN HOME PUSTAKA PEMDA SILAHKAN SESUIKAN. INGET!!! WAJIB CLINE CODE --}}
 {{-- HANDEL BY ALDO OR FAISAL --}}
 @extends('layouts.pustakapemdalayout')
 
@@ -15,7 +15,8 @@
                     </div>
                     <div class="">
                         <h6 class="font-bold">Alamat</h6>
-                        <p>Office : Jl. Sidomukti No 30, Kel. Kadipaten, Kec.Keraton, Kota Yogyakarta, Daerah Istimewa Yogyakarta (55132)</p>
+                        <p>Office : Jl. Sidomukti No 30, Kel. Kadipaten, Kec.Keraton, Kota Yogyakarta, Daerah Istimewa
+                            Yogyakarta (55132)</p>
                     </div>
                     <div class="">
                         <h6 class="font-bold">Email</h6>
@@ -23,18 +24,25 @@
                     </div>
                     <div class="flex flex-row gap-1 mt-3 md:mt-0 md:w-[250px]">
                         <a href="#">
-                            <i data-lucide="facebook" class="w-8 h-8 p-1 rounded-xl bg-[#BB271A] text-white hover:bg-white hover:text-[#BB271A] tranform transition-all flex items-center justify-center"></i>
+                            <i data-lucide="facebook"
+                                class="w-8 h-8 p-1 rounded-xl bg-[#BB271A] text-white hover:bg-white hover:text-[#BB271A] tranform transition-all flex items-center justify-center"></i>
                         </a>
                         <a href="#">
-                            <i data-lucide="instagram" class="w-8 h-8 p-1 rounded-xl bg-[#BB271A] text-white hover:bg-white hover:text-[#BB271A] tranform transition-all flex items-center justify-center"></i>
+                            <i data-lucide="instagram"
+                                class="w-8 h-8 p-1 rounded-xl bg-[#BB271A] text-white hover:bg-white hover:text-[#BB271A] tranform transition-all flex items-center justify-center"></i>
                         </a>
                         <a href="#">
-                            <i data-lucide="linkedin" class="w-8 h-8 p-2 rounded-xl bg-[#BB271A] text-white hover:bg-white hover:text-[#BB271A] tranform transition-all flex items-center justify-center"></i>
+                            <i data-lucide="linkedin"
+                                class="w-8 h-8 p-2 rounded-xl bg-[#BB271A] text-white hover:bg-white hover:text-[#BB271A] tranform transition-all flex items-center justify-center"></i>
                         </a>
                     </div>
                 </div>
-                <div class="overflow-hidden rounded-lg">
-                    <img src="/img/tentang1.png" alt="" loading="lazy" class=" object-contain md:max-w-full group-hover:scale-105 transform transition-all">
+                <div class="w-full mt-5 md:mt-0 flex justify-end">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247.05247688938937!2d110.36102033466555!3d-7.806796034091653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a5791fd206501%3A0xd1b9d44af39d9e21!2sJl.%20Sidomukti%20No.30!5e0!3m2!1sen!2sid!4v1753451268940!5m2!1sen!2sid"
+                        class="w-full max-w-[600px] h-[300px] md:h-[400px]" style="border:0;" allowfullscreen=""
+                        loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
                 </div>
             </div>
         </div>
