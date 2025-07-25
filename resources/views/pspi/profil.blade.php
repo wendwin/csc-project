db20ac0ac78fcb06e57c7324c5768b20105ae797
wendwin <wendwin12@gmail.com>
2025-07-25 20:16:02 +0700

    memperbaiki navbar

--- resources/views/pspi/profil.blade.php
+++ resources/views/pspi/profil.blade.php
@@ -4,8 +4,16 @@
 @extends('layouts.pspilayout')
 
 @section('content')
-<div class="text-center">
-    <div class="bg-white">
+<div class="text-center" x-data="{ isOpen: false, showTopbar: true }" 
+     x-init="
+        let lastScroll = window.pageYOffset;
+        window.addEventListener('scroll', () => {
+            const currentScroll = window.pageYOffset;
+            showTopbar = currentScroll === 0;
+            lastScroll = currentScroll;
+        });
+     ">
+    <div class="bg-white  transition-all duration-300 ease-linear"  :class="showTopbar ? 'md:mt-[105px] ' : 'md:mt-[115px]'">
         <div class="relative -mb-[100px] select-none pointer-events-none">
             <img src="/img/pspi/detail_berita.webp" alt="" class="w-full h-[250px] relative">
             <h1 class="absolute text-xl md:text-2xl text-white uppercase font-bold top-5 left-5 md:top-10 md:left-10">Profil Kami</h1>
@@ -15,4 +23,5 @@
     @include('pspi-components.profil.mengapa')
     @include('pspi-components.profil.visi_mitra')
     @include('pspi-components.profil.deskripsi')
+</div>
 @endsection
